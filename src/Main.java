58d6d240decdfa67a45daaa2b1ab56ce5692bfd5
User <madilovamairamkan@gmail.com>
2023-02-27 15:10:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+        UnaryOperator<Double> unaryOperator = s -> s.doubleValue();
+        System.out.print(unaryOperator.apply(Math.sqrt(36)));
+
+    }
+        public static UnaryOperator<Double> sqrt () {
+            return sqrt();
+        }
+
+        }
+
+
+
+
+
+
